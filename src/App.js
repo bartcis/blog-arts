7c88dbd44a6eb682c36507dcf7971b9b602c9d47
bartcis <cis.bart@gmail.com>
2020-01-16 22:34:12 +0000

    Abort request on load component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Articles} from './components/Articles';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Articles />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Articles.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import './Articles.scss';
+
+export const Articles = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const {signal} = abortController;
+
+    const apiCall = async path => {
+      try {
+        const request = await fetch(path, {
+          signal: signal,
+          method: 'GET',
+        });
+        const response = await request.json();
+        setState([response]);
+      } catch (e) {
+        if (!signal?.aborted) {
+          console.error(e);
+        }
+      }
+    };
+
+    apiCall('https://jsonplaceholder.typicode.com/posts/1');
+
+    return () => {
+      abortController.abort();
+    };
+  }, [setState]);
+
+  return (
+    <>
+      {state.map(article=> (
+        <article key={article?.id} className='article'>
+          <h1>{article?.title}</h1>
+          <p>{article?.body}</p>
+        </article> 
+      ))}
+    </>
+  );
+};
